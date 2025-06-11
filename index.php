0ee217dc777cceb779f2c9ddd3d68e6237f87281
tommaso.dare <tommaso.dare96@gmail.com>
2025-06-11 15:20:33 +0200

    Add Movie class implementation with constructor and methods

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<?php
+
+class Movie
+{
+
+    // Variabili d'istanza
+    public $title;
+    public $director;
+    public $release_year;
+    public $rating;
+    public $duration;
+
+    // Costruttore
+    function __construct($title, $director, $release_year, $rating, $duration)
+    {
+        $this->title = $title;
+        $this->director = $director;
+        $this->release_year = $release_year;
+        $this->rating = $rating;
+        $this->duration = $duration;
+    }
+
+    // Metodo per ottenere il titolo del film
+    public function getMovieTitle()
+    {
+        return $this->title;
+    }
+}
+
+$lotr = new Movie("Il Signore degli Anelli - La Compagnia dell'Anello", "Peter Jackson", 2001, 9.0, 178);
+$ironman = new Movie("Iron Man", "Jon Favreau", 2008, 7.9, 126);
+
+var_dump($lotr);
+var_dump($ironman);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +46,8 @@
 </head>
 
 <body>
+
+    <h1>Movie OOP</h1>
 
 </body>
 
